aed8a6d58ff472da3cd5198dcd334821251c72fd
Awais <awaisyaqoob350@gmail.com>
2023-12-06 16:32:21 +0500

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 
 
 
@@ -5,7 +6,7 @@ function App() {
  
   return (
     <>
-      <h1 className="text-3xl font-bold underline">this is app</h1>
+      <Hero/>
     </>
   )
 }
